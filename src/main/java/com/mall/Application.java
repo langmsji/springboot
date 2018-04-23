43edcd5cb4a2a7d75b2fed58377e1f6b238862d2
weepal <test.@126.com>
2018-04-23 17:49:32 +0800

    跨域 外部tomcat 运行

--- src/main/java/com/mall/Application.java
+++ src/main/java/com/mall/Application.java
@@ -3,11 +3,17 @@ package com.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.mall.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
--- src/main/java/com/mall/core/LoginInterceptor.java
+++ src/main/java/com/mall/core/LoginInterceptor.java
@@ -17,12 +17,23 @@ public class LoginInterceptor implements HandlerInterceptor{
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         //String requestURI = httpServletRequest.getRequestURI();
         HttpSession httpSession = httpServletRequest.getSession();
-        Object isLogin = httpSession.getAttribute("user1");
-        if(isLogin == null) {
-            PrintWriter out = httpServletResponse.getWriter();
-            out.print("{success:400,data:null,msg:\"用户未登录\"}");
-            //sendRedirect("/default");
+        if(httpServletRequest.getRequestURI().contains("/doRegister")||httpServletRequest.getRequestURI().contains("/doLogin")||httpServletRequest.getRequestURI().contains("/error")){
+
+        }else{
+            Object isLogin = httpSession.getAttribute("currentUser");
+            if(isLogin == null) {
+//            httpServletResponse.setCharacterEncoding("UTF-8");
+                httpServletResponse.setContentType("application/json; charset=utf-8");
+                PrintWriter out = httpServletResponse.getWriter();
+                out.print("{\"success\":400,\"data\":null,\"msg\":\"用户未登录\"}");
+                return false;
+                //sendRedirect("/default");
+            }
         }
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Authorization, Content-Type");
         return true;
     }
 
